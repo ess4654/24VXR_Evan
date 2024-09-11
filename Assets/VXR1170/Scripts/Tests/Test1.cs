a2aeddda0cd2af63c80f2d6a3f968cd6f0f7ec97
Evan S <thejoblesscoder@gmail.com>
2024-09-11 12:27:27 -0600

    Initial Commit

---
+++ Assets/VXR1170/Scripts/Tests/Test1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace VXR1170
+{
+    public class Test1 : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
