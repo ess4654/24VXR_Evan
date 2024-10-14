0ee2077b111b812070b17862a144465df2b55cb4
Evan S <thejoblesscoder@gmail.com>
2024-10-14 02:13:28 -0600

    Lab 4.2

---
+++ Assets/VXR1190/Scripts/AudioPlaybackManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlaybackManager : MonoBehaviour
+{
+    public AudioSource audioPlayer;
+
+    public void PlayAudio()
+    {
+        audioPlayer.Play();
+    }
+}
