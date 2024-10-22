0e5e7e7e4593c09e007de5a868469d3e768e0c66
Evan S <thejoblesscoder@gmail.com>
2024-10-22 15:24:50 -0600

    Added assets for Greyboxing.

---
+++ Assets/Shared/Scripts/Singleton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Shared
+{
+    /// <summary>
+    ///     Base class used by all singleton manager scripts.
+    /// </summary>
+    /// <typeparam name="TClass">Type of class inherited by this singleton.</typeparam>
+    public abstract class Singleton<TClass> : MonoBehaviour
+    where TClass : Singleton<TClass>
+    {
+        /// <summary>
+        ///     Reference to the instance of the singleton.
+        /// </summary>
+        public static TClass Instance { get; private set; }
+
+
+        protected virtual void Awake()
+        {
+            if(Instance != null && Instance != GetComponent<TClass>())
+            {
+                Destroy(GetComponent<TClass>());
+                return;
+            }
+
+            Instance = GetComponent<TClass>();
+        }
+    }
+}
