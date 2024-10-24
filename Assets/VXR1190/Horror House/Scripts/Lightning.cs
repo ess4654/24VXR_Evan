32c66617ebdb352c67857c2f6560bdbac2c9e86e
Evan S <thejoblesscoder@gmail.com>
2024-10-23 23:51:25 -0600

    House prepped for starting on the Sound Scape Plan. Religious artifacts removed.

---
+++ Assets/VXR1190/Horror House/Scripts/Lightning.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace HorrorHouse
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class Lightning : MonoBehaviour
+    {
+        [SerializeField] private Light lightingFlash;
+        [SerializeField] private AudioSource thunderSound;
+        [SerializeField] private AudioClip[] thunderAudioClips;
+
+        private ParticleSystem lighting;
+        private bool on;
+
+        private void Awake()
+        {
+            lighting = GetComponent<ParticleSystem>();
+        }
+
+        private void Update()
+        {
+            if (!on && lighting.particleCount > 0)
+            {
+                on = true;
+                if (thunderSound && thunderAudioClips.Length > 0)
+                    thunderSound.PlayOneShot(thunderAudioClips[Random.Range(0, thunderAudioClips.Length)]);
+            }
+            else if (on && lighting.particleCount == 0)
+            {
+                on = false;
+                if (lightingFlash)
+                    lightingFlash.gameObject.SetActive(false);
+            }
+
+            if (on)
+                OnLighting();
+        }
+
+        private void OnLighting()
+        {
+            if (lightingFlash && Random.value < .2f)
+                lightingFlash.gameObject.SetActive(!lightingFlash.gameObject.activeInHierarchy);
+        }
+    }
+}
