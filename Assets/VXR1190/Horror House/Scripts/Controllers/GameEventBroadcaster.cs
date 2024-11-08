f23eb7ce5b96360dcc010d50a17de687b6efdc76
Evan S <thejoblesscoder@gmail.com>
2024-11-08 15:13:08 -0700

    Added leaves sound effect to outside

---
+++ Assets/VXR1190/Horror House/Scripts/Controllers/GameEventBroadcaster.cs
@@ -0,0 +1,23 @@
+namespace HorrorHouse.Controllers
+{
+    /// <summary>
+    ///     Broadcasts game events that classes can subscribe to for the horror house game.
+    /// </summary>
+    public static class GameEventBroadcaster
+    {
+        #region DELEGATES
+
+
+        #endregion
+
+        #region SUBSCRIBABLES
+
+
+        #endregion
+
+        #region BROADCASTERS
+
+
+        #endregion
+    }
+}
