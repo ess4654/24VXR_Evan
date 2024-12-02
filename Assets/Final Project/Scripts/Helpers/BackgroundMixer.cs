637df60370fb5a688c65fe5818eeb93ab3dddd41
Evan S <thejoblesscoder@gmail.com>
2024-12-02 07:52:04 -0700

    Added background music tracks to the project. Coded the mixer group for background music

---
+++ Assets/Final Project/Scripts/Helpers/BackgroundMixer.cs
@@ -0,0 +1,101 @@
+using ArcadeGame.Controllers;
+using ArcadeGame.Data;
+using Shared;
+using Shared.Editor;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace ArcadeGame.Helpers
+{
+    /// <summary>
+    ///     Constrols the audio mixer for the background music of the arcade game.
+    /// </summary>
+    public class BackgroundMixer : Singleton<BackgroundMixer>
+    {
+        #region VARIABLE DECLARATIONS
+
+        [SerializeField] private AudioMixerSnapshot[] snapshots;
+        [SerializeField] private float transitionTime = 1f;
+
+        private AudioMixerSnapshot currentSnapshot;
+        private const string PitchParam = "Music Pitch";
+        private const float PitchNormal = 1f;
+        private const float PitchPaused = .97f;
+
+        #endregion
+
+        #region SETUP
+
+        protected override void Awake()
+        {
+            base.Awake();
+            currentSnapshot = snapshots[0];
+        }
+
+        private void OnEnable()
+        {
+            GameEventBroadcaster.OnGameStateChanged += SwitchTrack;
+        }
+
+        private void OnDisable()
+        {
+            GameEventBroadcaster.OnGameStateChanged -= SwitchTrack;
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        ///     Transitions to the new audio mixer snapotshot.
+        /// </summary>
+        /// <param name="snapshot">Snapshot to transition to.</param>
+        /// <param name="pitch">Pitch of the music tracks.</param>
+        private void TransitionTo(AudioMixerSnapshot snapshot, float pitch)
+        {
+            if(snapshot != null)
+            {
+                snapshot.TransitionTo(transitionTime);
+                snapshot.audioMixer.GetFloat(PitchParam, out float currentPitch);
+                LeanTween
+                    .value(currentPitch, pitch, transitionTime)
+                    .setOnUpdate((float _pitch) => snapshot.audioMixer.SetFloat(PitchParam, _pitch))
+                    .setEaseLinear();
+            }
+        }
+
+        /// <summary>
+        ///     Swaps the audio track when the game state has been changed.
+        /// </summary>
+        /// <param name="gameState">New Game State</param>
+        private void SwitchTrack(GameState gameState)
+        {
+            // Arcade = 0
+            // Cyclone = 1 & 6
+            // BigWheel = 2
+            // PirateSpin = 3
+            // ShooterCabinet = 4
+            // ClawMachine = 5
+
+            int index = (int)gameState;
+            float pitch = PitchNormal;
+            if (index > -1)
+                currentSnapshot = snapshots[index];
+            else
+                pitch = PitchPaused; //lower the pitch when paused
+            
+            TransitionTo(currentSnapshot, pitch);
+        }
+
+        #endregion
+
+        #region DEBUGGING
+
+        [Debugging]
+        [SerializeField] GameState gameTrack;
+        [SerializeField, InspectorButton("TestTransition")] bool m_TestTransition;
+        void TestTransition() => SwitchTrack(gameTrack);
+
+        #endregion
+    }
+}
